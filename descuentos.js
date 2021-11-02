dbd6d731143b27911febf463306fe947fda84fc3
Yorman Amaro <amarocarrero24@gmail.com>
2021-11-02 18:11:00 -0300

    Function descuento created

--- descuentos.js
+++ descuentos.js
@@ -1,2 +1,19 @@
-alert("Hola Mundo");
+//const precioOriginal = 120;
+//const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento)
+{
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+};
+
+
+//console.log({
+//    precioOriginal,
+//    porcentajePrecioConDescuento,
+//    precioConDescuento
+//});
+
 
