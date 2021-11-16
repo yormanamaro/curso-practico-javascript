4c42b5cfc13b428dc2473619ef2f615d2618d3f0
Yorman Amaro <amarocarrero24@gmail.com>
2021-11-16 16:47:08 -0300

    The discount project is finish

--- descuentos.js
+++ descuentos.js
@@ -7,7 +7,7 @@ function calcularPrecioConDescuento(precio, descuento)
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
-};
+}
 
 
 //console.log({
@@ -16,4 +16,17 @@ function calcularPrecioConDescuento(precio, descuento)
 //    precioConDescuento
 //});
 
+function onClickButtonPriceDiscount() 
+{
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value; 
 
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;  
+}
